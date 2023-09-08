b16f1ccf2ca8813de90cd131a4f983c69196476a
sizheng3 <sizheng3@fa23-cs425-1801.cs.illinois.edu>
2023-09-07 22:21:13 -0500

    added tests and comments

--- internals/server/server.go
+++ internals/server/server.go
@@ -12,11 +12,12 @@ import (
 )
 
 const (
-	HOST     = "0.0.0.0"
-	PORT     = "55555"
+	HOST = "0.0.0.0"
+	PORT = "55555"
 )
 
 func Start() {
+	//start monitoring requests on port 55555
 	server, err := net.Listen("tcp", HOST+":"+PORT)
 	if err != nil {
 		fmt.Printf("Error listening: %v\n", err.Error())
@@ -24,17 +25,20 @@ func Start() {
 	}
 	defer server.Close()
 	for {
+		//accept connection
 		conn, err := server.Accept()
 		if err != nil {
 			fmt.Printf("Error accepting: %v\n", err.Error())
 			os.Exit(1)
 		}
+		//start another goroutine to process information sent to conn
 		go serveConn(conn)
 	}
 }
 
 func serveConn(conn net.Conn) {
 	defer conn.Close()
+	//read content from conn
 	reader := bufio.NewReader(conn)
 	request, err := reader.ReadString('\n')
 	if err != nil {
@@ -44,8 +48,9 @@ func serveConn(conn net.Conn) {
 		return
 	}
 	request = strings.TrimSuffix(request, "\n")
-
+	//process the request
 	response := processRequest(request)
+	//write the response to client via conn
 	_, err = conn.Write(response)
 	if err != nil {
 		fmt.Printf("Error sending response: %v\n", err.Error())
@@ -56,10 +61,12 @@ func serveConn(conn net.Conn) {
 func processRequest(request string) []byte {
 	fmt.Println("Received request: ", request)
 	requestComponents := strings.Split(request, " ")
+	//for MP1, we won't handle any other things excep for grep
 	if requestComponents[0] != "grep" {
 		return []byte("Error: Invalid request. Please try again.\n")
 	}
 	startTime := time.Now()
+	//execute the requested command on current server
 	cmd := exec.Command("bash", "-c", request)
 	out, err := cmd.Output()
 	if err != nil {
