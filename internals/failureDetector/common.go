2ec52794a2b750fdfb77db5efa9af2591a6a3cf2
jyuan19 <jyuan19@illinois.edu>
2023-09-23 22:17:10 -0500

    increasing +s mode gossip frequency

--- internals/failureDetector/common.go
+++ internals/failureDetector/common.go
@@ -10,7 +10,7 @@ import (
 
 const (
 	INTRODUCER_ADDRESS  = "fa23-cs425-1801.cs.illinois.edu:55556"
-	GOSSIP_RATE         = 500 * time.Millisecond // 500 ms
+	GOSSIP_RATE         = 400 * time.Millisecond // 500 ms
 	T_FAIL              = 2 * time.Second        // 2 seconds
 	T_SUSPECT           = 2 * time.Second        // 2 seconds
 	T_CLEANUP           = 3 * time.Second        // 3 seconds
